9c90fab4167de291c63310292db0c7eb430751ee
atayabadenmark-hub <atayabadenmark@gmail.com>
2026-08-26 09:00:59 +0800

    Update code

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import  java.util.Scanner;
 public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     System.out.println("===JAVA ACTIVITY PORTFOLIO===");
-    System.out.println("==Name: lord");
+    System.out.println("==Name: DenS");
     System.out.println("==Section/Year: BSIT 2A");
     System.out.println("=======CONTENTS=======");
     System.out.println("1. Activity 1");
